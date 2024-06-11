d85dfe0d8517da230ba688882b9cbc91be6ce0f5
Puneet-Kumar2010 <126220134+Puneet-Kumar2010@users.noreply.github.com>
2024-06-11 14:36:36 +0530

    alpha version

--- public/js/DeveloperContact.js
+++ public/js/DeveloperContact.js
@@ -4,18 +4,22 @@ const openBtn = document.getElementById("open-btn");
 const section1 = document.getElementById("section-1");
 
 closeBtn.addEventListener("click", () => {
-    menu.style.right = "-400px"
+    menu.style.right = "-350px";
+    menu.style.display: "none";
 });
 
 openBtn.addEventListener("click", () => {
     menu.style.right = "0"
     menu.style.width = "350px";
+    menu.style.display: "block";
 });
 
 document.addEventListener("scroll", () => {
-    menu.style.right = "-400px"
+    menu.style.right = "-350px"
+    menu.style.display: "none";
 });
 
 section1.addEventListener("click", () => {
-    menu.style.right = "-400px"
+    menu.style.right = "-350px"
+    menu.style.display: "none";
 });
