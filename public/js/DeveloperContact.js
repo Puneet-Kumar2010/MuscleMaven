56146c9fb725a87b298657ac0fc210808678fdfa
Puneet-Kumar2010 <126220134+Puneet-Kumar2010@users.noreply.github.com>
2024-06-13 12:00:50 +0000

    alpha version

--- public/js/DeveloperContact.js
+++ public/js/DeveloperContact.js
@@ -3,6 +3,8 @@ const menu = document.getElementById("options");
 const menuDiv = document.querySelectorAll(".options div");
 const openBtn = document.getElementById("open-btn");
 const section1 = document.getElementById("section-1");
+const content = document.getElementById("message");
+const mail = document.getElementById("mail-content");
 
 closeBtn.addEventListener("click", () => {
     menu.style.right = "-350px";
@@ -19,3 +21,11 @@ document.addEventListener("scroll", () => {
 section1.addEventListener("click", () => {
     menu.style.right = "-350px"
 });
+
+content.addEventListener("input", ()=>{
+    mail.value = content.value;
+});
+
+setInterval(()=>{
+    mail.value = content.value;
+}, 1);
